80cf56eea776980f193bfa21beb9f848524f3c4e
keneskhqn <alkhaydar17@icloud.com>
2025-01-24 22:56:31 +0500

    ufc229

---
+++ 19.py
@@ -0,0 +1,18 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict["brand"])
+
+for x in thisdict:
+  print(thisdict[x])
+
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict = dict(thisdict)
+print(mydict)
